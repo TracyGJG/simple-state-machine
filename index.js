0789d03a0a67531f4799de2c7f472342eac8bd0a
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-09-13 21:33:00 +0100

    Model externalised and testing extended

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-export default function StateMachine(stateModel) {
+export default function StateMachine(stateModel, actions) {
   let _currentState = Object.keys(stateModel)[0];
 
   return {
-    triggerEvent(trigger, payload) {
-      if (stateModel[_currentState].triggers[trigger]) {
-        const activeState = _currentState;
-        _currentState = stateModel[activeState].triggers[trigger];
-        return stateModel[activeState].action(payload);
+    triggerEvent(_trigger, _payload) {
+      if (stateModel[_currentState].triggers[_trigger]) {
+        _currentState = stateModel[_currentState].triggers[_trigger];
+        const _action = stateModel[_currentState].action;
+
+        if (!actions[_action]) {
+          throw Error(
+            `Unable to locate action '${_action}' for current state (${_currentState}).`
+          );
+        }
+        return actions[_action](_payload);
       }
       throw Error(
-        `Unable to locate trigger '${trigger}' for current state (${_currentState}).`
+        `Unable to locate trigger '${_trigger}' for current state (${_currentState}).`
       );
     },
     currentState: () => _currentState,
--- index.test.js
+++ index.test.js
@@ -1,35 +1,19 @@
 import { describe, test, mock } from 'node:test';
 import assert from 'node:assert/strict';
 
+import stateModel from './stateModel.json' with { type:  'json'};
+
 import StateMachine from './index.js';
 
+const actions = {
+  initialAction: payload => `initial::Payload: "${payload}"`,
+  ticAction: payload => `tic::Payload: "${payload}"`,
+  tacAction: payload => `tac::Payload: "${payload}"`,
+  tocAction: payload => `toe::Payload: "${payload}"`,
+};
+
 describe('StateMachine', () => {
-  const { triggerEvent, currentState } = StateMachine({
-    initial: {
-      action: payload => `initial::Payload: "${payload}"`,
-      triggers: {
-        next: 'tic',
-      },
-    },
-    tic: {
-      action: payload => `tic::Payload: "${payload}"`,
-      triggers: {
-        next: 'tac',
-        again: 'tic',
-      },
-    },
-    tac: {
-      action: payload => `tac::Payload: "${payload}"`,
-      triggers: {
-        next: 'toe',
-        back: 'tic',
-      },
-    },
-    toe: {
-      action: payload => `toe::Payload: "${payload}"`,
-      triggers: {},
-    },
-  });
+  const { triggerEvent, currentState } = StateMachine(stateModel, actions);
 
   test('can provide the initial state', () => {
     assert.equal(currentState(), 'initial');
@@ -45,7 +29,7 @@ describe('StateMachine', () => {
   test('can provide the updated state', () => {
     assert.equal(currentState(), 'initial');
     const result = triggerEvent('next', 'next from initial to tic');
-    assert.equal(result, 'initial::Payload: "next from initial to tic"');
+    assert.equal(result, 'tic::Payload: "next from initial to tic"');
     assert.equal(currentState(), 'tic');
 
     triggerEvent('again', 'again from tic to tic');
@@ -63,4 +47,19 @@ describe('StateMachine', () => {
     triggerEvent('next', 'next from tac to toe');
     assert.equal(currentState(), 'toe');
   });
+});
+
+describe('StateMachine (invalid action)', () => {
+  const _stateModel = structuredClone(stateModel);
+    _stateModel.tic.action = '_';
+
+  const { triggerEvent, currentState } = StateMachine(_stateModel, actions);
+
+  test('throws an exception for unlocated action', () => {
+    assert.equal(currentState(), 'initial');
+    assert.throws(
+      () => triggerEvent('next', 'next from initial to tic'),
+      Error("Unable to locate action '_' for current state (tic).")
+    );
+  })
 });
